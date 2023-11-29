2e33b12823748e37f1b4fb3cc3db5895a8b9d444
w-modz <46196436+w-modz@users.noreply.github.com>
2023-11-29 15:39:19 +0100

    Implement matrix multiplication and progress bar updates

--- MatrixMultiplier/Form1.Designer.cs
+++ MatrixMultiplier/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            progressBar = new ProgressBar();
+            backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            buttonStart = new Button();
+            labelDone = new Label();
+            SuspendLayout();
+            // 
+            // progressBar
+            // 
+            progressBar.ForeColor = Color.FromArgb(128, 255, 128);
+            progressBar.Location = new Point(12, 12);
+            progressBar.MarqueeAnimationSpeed = 1;
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(776, 58);
+            progressBar.Step = 1;
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.TabIndex = 0;
+            // 
+            // backgroundWorker
+            // 
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Anchor = AnchorStyles.Top;
+            buttonStart.Location = new Point(305, 76);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(188, 58);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // labelDone
+            // 
+            labelDone.AutoSize = true;
+            labelDone.Location = new Point(12, 85);
+            labelDone.Name = "labelDone";
+            labelDone.Size = new Size(99, 41);
+            labelDone.TabIndex = 2;
+            labelDone.Text = "Done!";
+            labelDone.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelDone);
+            Controls.Add(buttonStart);
+            Controls.Add(progressBar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private Button buttonStart;
+        private Label labelDone;
     }
 }
--- MatrixMultiplier/Form1.cs
+++ MatrixMultiplier/Form1.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace MatrixMultiplier
 {
     public partial class Form1 : Form
@@ -5,6 +7,78 @@ namespace MatrixMultiplier
         public Form1()
         {
             InitializeComponent();
+            backgroundWorker.WorkerReportsProgress = true;
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            labelDone.Visible = false;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        public static int[,] GenerateRandomMatrix(int n)
+        {
+            // Create a new random number generator.
+            Random random = new Random();
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next();
+                }
+            }
+
+            return matrix;
+        }
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            int[,] A = GenerateRandomMatrix(1000);
+            int[,] B = GenerateRandomMatrix(1000);
+            int percentage = 0;
+            int index = 0;
+
+            int rA = A.GetLength(0);
+            int cA = A.GetLength(1);
+            int rB = B.GetLength(0);
+            int cB = B.GetLength(1);
+
+            if (cA != rB)
+            {
+                MessageBox.Show("Matrixes can't be multiplied!!");
+            }
+            else
+            {
+                int temp = 0;
+                int[,] kHasil = new int[rA, cB];
+
+                for (int i = 0; i < rA; i++)
+                {
+                    for (int j = 0; j < cB; j++)
+                    {
+                        temp = 0;
+                        for (int k = 0; k < cA; k++)
+                        {
+                            temp += A[i, k] * B[k, j];
+                        }
+                        kHasil[i, j] = temp;
+                        index++;
+                        percentage = (index) * 100 / (rA * cB);
+                        backgroundWorker.ReportProgress(percentage);
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            labelDone.Visible = true;
         }
     }
 }
